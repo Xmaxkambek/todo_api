c2461d68a3bfd4a9f2b3820ce6a41fcfe1eac083
ZARIFMAC <uzbzarif@gmail.com>
2022-12-22 16:12:03 +0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TodoAPP());
+}
+
+class TodoAPP extends StatefulWidget {
+  const TodoAPP({Key? key}) : super(key: key);
+
+  @override
+  State<TodoAPP> createState() => _TodoAPPState();
+}
+
+class _TodoAPPState extends State<TodoAPP> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
