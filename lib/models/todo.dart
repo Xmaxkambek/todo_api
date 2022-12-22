8925eba086fae4ffe007f624f0ebf20f0f057089
ZARIFMAC <uzbzarif@gmail.com>
2022-12-22 16:11:21 +0500

    model

---
+++ lib/models/todo.dart
@@ -0,0 +1,11 @@
+class Todo {
+  final String title;
+  final String description;
+  final bool completed;
+
+  Todo({
+    required this.title,
+    required this.description,
+    required this.completed,
+  });
+}
