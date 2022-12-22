a2e9f792faf42e2e5a727c78176fab6bbf4b7766
ZARIFMAC <uzbzarif@gmail.com>
2022-12-22 16:37:52 +0500

    get task

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -8,4 +8,12 @@ class Todo {
     required this.description,
     required this.completed,
   });
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      title: json['title'],
+      description: json['description'],
+      completed: json['completed'],
+    );
+  }
 }
---
+++ lib/services/todoAPI.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:todo_api/models/todo.dart';
+
+Future<List<Todo>> getTodo() async {
+  String baseUrl = "https://motof.pythonanywhere.com/";
+  Uri url = Uri(
+    scheme: 'https',
+    host: 'motof.pythonanywhere.com',
+    path: 'get',
+  );
+  // Make a request to the API
+  final response = await http.get(url);
+  // Check if the request was successful
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    List<Todo> todos = [];
+
+    for (var todo in jsonDecode(response.body)) {
+      todos.add(Todo.fromJson(todo));
+    }
+    return todos;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load todo');
+  }
+}
